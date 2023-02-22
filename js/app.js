ab4b801a04829a334433b68bb73d67ecc5d6e10a
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-02-22 17:07:46 +0100

    add header placeholders

--- js/app.js
+++ js/app.js
@@ -2,7 +2,26 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      links: ["Home", "About", "Services", "Training", "Blogs"],
+      contacts: [
+        {
+          info: "info@example.com",
+          icon: "placeholder",
+        },
+        {
+          info: "+1(817)9013377",
+          icon: "placeholder",
+        },
+      ],
+      logos: [
+        "../img/youtube-logo.svg",
+        "../img/youtube-logo.svg",
+        "../img/youtube-logo.svg",
+        "../img/youtube-logo.svg",
+      ],
+      brand: ["../img/logo.png"],
+    };
   },
 });
 
